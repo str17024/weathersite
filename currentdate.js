b322fe6e74649e61f2e9e660db64d242609f5cd6
Ken Strobell <35313618+str17024@users.noreply.github.com>
2018-03-15 17:33:37 -0600

    Add files via upload

---
+++ currentdate.js
@@ -0,0 +1,42 @@
+var today = new Date();
+var wd = today.getDay();
+var dd = today.getDate();
+var mm = today.getMonth(); //January is 0!
+var yyyy = today.getFullYear();
+
+var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+	
+var month = new Array(7);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";		
+	
+var n = weekday[wd];
+var p = month[mm];
+
+if (dd < 10) {
+    dd = '0' + dd
+} 
+
+if(mm < 10) {
+    mm = '0' + mm
+} 
+
+today = n + '/' + p + dd + '/' + yyyy;
+document.write(today);
---
+++ hamburger.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+	//console.log(document.getElementById("primaryNav").classlist;
+	document.getElementById("primaryNav").classList.toggle("hide");
+	
+}
